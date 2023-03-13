2455b65095d26ca997f548eaa23015d3aa20f777
Dima <Dima@DESKTOP-8CJJBIF>
2023-03-13 20:22:54 +0300

    Добавьте файлы проекта.

---
+++ 23/23.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include "Class.h"
+
+using namespace std;
+
+void Test() {
+    Point x(6.5, 9.04);
+    Point y(-4, 1.3);
+
+    x = x * 5;
+    y = x + y;
+
+    x.Show();
+    y.Show();
+    if (x != y)
+        cout << "FALSE\n\n";
+}
+
+int main() {
+    
+    Test();
+}
